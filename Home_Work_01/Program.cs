e23545bc6bb22b0c2bd24945aabefd42ea261a69
Max Babich <sapienti@list.ru>
2022-12-17 15:19:14 +0300

    task3 added

--- Home_Work_01/Program.cs
+++ Home_Work_01/Program.cs
@@ -12,10 +12,8 @@ int secondNamber = Convert.ToInt32 (Console.ReadLine());
             Console.WriteLine("Your numbers are equal");    
         else
             Console.WriteLine("Your second namber is greater than the first");*/
-
     
-    
-    
+        
     /* Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт
     максимальное из этих чисел.*/
 
@@ -37,11 +35,19 @@ int max = 0;
 
 Console.WriteLine("The largest namber is " + max);*/
         
-
-
-
-    /* Задача 3. Напишите программу, которая на вход принимает число и выдаёт,
+     
+     /* Задача 3. Напишите программу, которая на вход принимает число и выдаёт,
     является ли число чётным (делится ли оно на два без остатка).*/
+
+/*int rem = 0;
+Console.Write ("Enter the namber: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+rem =  namber % 2;
+    if (rem==0)
+        Console.WriteLine("The namber is even");
+    else
+        Console.WriteLine ("The namber is not even");*/
+
 
     /* Задача 4. Напишите программу, которая на вход принимает число (N),
     а на выходе показывает все чётные числа от 1 до N.*/
