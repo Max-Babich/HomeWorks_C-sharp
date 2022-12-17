ebaeed14377ba301f699d83409bafd282ab7e493
Max Babich <sapienti@list.ru>
2022-12-17 15:47:34 +0300

    task4 added

--- Home_Work_01/Program.cs
+++ Home_Work_01/Program.cs
@@ -1,7 +1,8 @@
 ﻿    /* Задача 1. Напишите программу, которая на вход принимает два числа и выдаёт,
     какое число большее, а какое меньшее.*/
 
-/*Console.Write ("Enter the first namber: ");
+/*
+Console.Write ("Enter the first namber: ");
 int firstNamber = Convert.ToInt32 (Console.ReadLine());
 Console.Write ("Enter the second namber: ");
 int secondNamber = Convert.ToInt32 (Console.ReadLine());
@@ -11,13 +12,15 @@ int secondNamber = Convert.ToInt32 (Console.ReadLine());
         if (firstNamber==secondNamber)
             Console.WriteLine("Your numbers are equal");    
         else
-            Console.WriteLine("Your second namber is greater than the first");*/
+            Console.WriteLine("Your second namber is greater than the first");
+*/
     
         
     /* Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт
     максимальное из этих чисел.*/
 
-/*Console.Write ("Enter the first namber: ");
+/*
+Console.Write ("Enter the first namber: ");
 int firstNamber = Convert.ToInt32 (Console.ReadLine());
 Console.Write ("Enter the second namber: ");
 int secondNamber = Convert.ToInt32 (Console.ReadLine());
@@ -33,21 +36,36 @@ int max = 0;
             if (firstNamber<thirdNamber)
                 max = thirdNamber;
 
-Console.WriteLine("The largest namber is " + max);*/
+Console.WriteLine("The largest namber is " + max);
+*/
         
      
      /* Задача 3. Напишите программу, которая на вход принимает число и выдаёт,
     является ли число чётным (делится ли оно на два без остатка).*/
 
-/*int rem = 0;
+/*
+int rem = 0;
 Console.Write ("Enter the namber: ");
 int namber = Convert.ToInt32(Console.ReadLine());
 rem =  namber % 2;
     if (rem==0)
         Console.WriteLine("The namber is even");
     else
-        Console.WriteLine ("The namber is not even");*/
+        Console.WriteLine ("The namber is not even");
+*/
 
 
     /* Задача 4. Напишите программу, которая на вход принимает число (N),
     а на выходе показывает все чётные числа от 1 до N.*/
+
+/*
+int i = 2;
+Console.Write ("Enter the namber: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+    while ( i < namber)
+    {
+        Console.Write (i + ", ");
+        i = i+2;
+
+    }
+*/
