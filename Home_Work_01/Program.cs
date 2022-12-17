44cd4f0f4b5d3ccc65b0ec099d92b542935ec434
Max Babich <sapienti@list.ru>
2022-12-17 14:45:47 +0300

    task2 added

--- Home_Work_01/Program.cs
+++ Home_Work_01/Program.cs
@@ -5,19 +5,39 @@
 int firstNamber = Convert.ToInt32 (Console.ReadLine());
 Console.Write ("Enter the second namber: ");
 int secondNamber = Convert.ToInt32 (Console.ReadLine());
-if (firstNamber>secondNamber)
-    Console.WriteLine("Your first number is greater than the second");
+    if (firstNamber>secondNamber)
+        Console.WriteLine("Your first number is greater than the second");
     else
         if (firstNamber==secondNamber)
-        Console.WriteLine("Your numbers are equal");    
+            Console.WriteLine("Your numbers are equal");    
         else
-        Console.WriteLine("Your second namber is greater than the first");*/
+            Console.WriteLine("Your second namber is greater than the first");*/
 
     
     
     
     /* Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт
     максимальное из этих чисел.*/
+
+/*Console.Write ("Enter the first namber: ");
+int firstNamber = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Enter the second namber: ");
+int secondNamber = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Enter the third namber: ");
+int thirdNamber = Convert.ToInt32 (Console.ReadLine());
+int max = 0;
+    if (firstNamber>secondNamber)
+        max = firstNamber;
+    else
+        if (secondNamber>thirdNamber)
+            max = secondNamber;
+        else
+            if (firstNamber<thirdNamber)
+                max = thirdNamber;
+
+Console.WriteLine("The largest namber is " + max);*/
+        
+
 
 
     /* Задача 3. Напишите программу, которая на вход принимает число и выдаёт,
