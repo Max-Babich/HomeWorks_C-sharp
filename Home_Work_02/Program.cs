939470f03ed831e50787430c62cde6a8b57ab41e
Max Babich <sapienti@list.ru>
2022-12-23 19:44:58 +0300

    second task added

--- Home_Work_02/Program.cs
+++ Home_Work_02/Program.cs
@@ -24,9 +24,29 @@
 
 /*Задача 2: Напишите программу, которая выводит третью цифру заданного числа
  или сообщает, что третьей цифры нет.*/
+/*
+int cutNumber(int number)
+{
+   while (number >= 1000)
+      {
+         number = number/10;
+      }
+   int result = number%10;
+   return result;
 
-
-
-
+}
+Console.Write("Enter the number: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+   
+   if (number>99)
+      {
+         int answer = cutNumber(number);
+         Console.Write($"The third digit of the left is {answer}.");   
+      }   
+   else
+      Console.Write("There is no third digit in the number");
+*/      
+         
+         
 /*Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
  и проверяет, является ли этот день выходным.*/
