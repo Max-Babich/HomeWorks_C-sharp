55f66c66bc87198ef334970ab30b43b1110a5ba0
Max Babich <sapienti@list.ru>
2022-12-24 18:01:44 +0300

    first task added

---
+++ Home_Work_03/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 1. Напишите программу, которая принимает на вход число и проверяет,
+ является ли оно палиндромом.*/
+/*
+int result = 0;
+int reversNumber (int number)
+{   
+    while (number > 0)
+        {
+           int reminder = number % 10;
+            number = number / 10;
+            result = result * 10 + reminder;
+        }
+    int finResult = result;
+    return finResult;
+}
+ 
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int oper = number;
+    
+    if (oper == reversNumber(number)) 
+        Console.WriteLine("Your number is a palindrome.");
+    else
+        Console.WriteLine("Your number is not a palindrome.");
+*/
+        
+        
+
+/*Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит
+  расстояние между ними в 3D пространстве.*/
+
+
+
+
+/*Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов
+  чисел от 1 до N.*/
