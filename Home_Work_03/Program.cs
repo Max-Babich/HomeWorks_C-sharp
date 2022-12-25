9234329c7d3d024faf1619a74108405c4891db11
Max Babich <sapienti@list.ru>
2022-12-25 12:21:20 +0300

    second task added

--- Home_Work_03/Program.cs
+++ Home_Work_03/Program.cs
@@ -29,6 +29,27 @@ int oper = number;
 /*Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит
   расстояние между ними в 3D пространстве.*/
 
+void distans3d(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+  double distans = Math.Round(Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2)),2);
+  Console.Write($"Distance between points = {distans}");
+}
+
+Console.WriteLine("Enter the coordinates of the first point");
+Console.Write("x1 = ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y1 = ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("z1 = ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the coordinates of the second point");
+Console.Write("x2 = ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y2 = ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("z2 = ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+ distans3d(x1, x2, y1, y2, z1, z2);
 
 
 
