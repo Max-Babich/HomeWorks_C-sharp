9d72d79d2dfb42dd57dd83d65543de8244196c32
Max Babich <sapienti@list.ru>
2022-12-25 12:23:47 +0300

    litle edit

--- Home_Work_03/Program.cs
+++ Home_Work_03/Program.cs
@@ -28,7 +28,7 @@ int oper = number;
 
 /*Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит
   расстояние между ними в 3D пространстве.*/
-
+/*
 void distans3d(double x1, double x2, double y1, double y2, double z1, double z2)
 {
   double distans = Math.Round(Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2)),2);
@@ -50,7 +50,7 @@ double y2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("z2 = ");
 double z2 = Convert.ToDouble(Console.ReadLine());
  distans3d(x1, x2, y1, y2, z1, z2);
-
+*/
 
 
 /*Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов
