cc64eafe6eadaee838d0807c91f2e14661efdf69
Max Babich <sapienti@list.ru>
2023-01-26 13:49:59 +0300

    HW4 added

---
+++ HomeWorks_04/Program.cs
@@ -0,0 +1,21 @@
+﻿/*    Задача 1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число
+ A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+/*
+int getPow(int firstNum, int secondNum)
+{
+    int pow =1;
+    for (int current=1; current<=secondNum; current++)
+    pow*=firstNum;
+    return pow;
+}
+Console.Write("Input the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The {a} to the {b} power is {getPow(a,b)}.");
+*/
+
