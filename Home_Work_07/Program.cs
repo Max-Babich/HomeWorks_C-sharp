8ef5259bd1980ebc951aae3cff9a87350015c3f2
Max Babich <sapienti@list.ru>
2023-02-03 14:26:46 +0300

    task 2 added

--- Home_Work_07/Program.cs
+++ Home_Work_07/Program.cs
@@ -6,8 +6,17 @@ m = 3, n = 4.
 */
 
 /*
-double[,] Random2dArray (int rows, int columns, int minVal, int maxVal)
-{
+double[,] Random2dArray ()
+{   
+    Console.Write("Enter the minimum value of random range: ");
+    int minVal = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the maximum value of random range: ");
+    int maxVal = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
     double[,] array = new double[rows, columns];
     for(int i = 0; i < rows; i++)
         for (int j=0; j < columns; j++)
@@ -23,20 +32,13 @@ void Show2dArray(double [,] array)
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write(array [i,j] + "; ");
+            Console.Write(array [i,j] + "\t");  
         Console.WriteLine();
     }
+ Console.WriteLine();   
 }
 
-Console.Write("Enter the minimum value of random range: ");
-int minVal = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the maximum value of random range: ");
-int maxVal = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the number of rows: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the number of columns: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-double[,] newArray = Random2dArray(rows, columns, minVal, maxVal);
+double[,] newArray = Random2dArray();
 Show2dArray(newArray);
 */
 
@@ -48,6 +50,50 @@ Show2dArray(newArray);
 8 4 2 4
 1 7 -> элемента с такими индексами в массиве нет
 */
+
+double[,] Random2dArray ()
+{   
+    Console.Write("Enter the minimum value of random range: ");
+    int minVal = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the maximum value of random range: ");
+    int maxVal = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the numbers of rows in the array: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the numbers of columns in the array: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    double[,] array = new double[rows, columns];
+    for(int i = 0; i < rows; i++)
+        for (int j=0; j < columns; j++)
+        {
+            array [i,j] = Math.Round (new Random().NextDouble () + new Random().Next(minVal, maxVal), 2);
+            
+        }
+    return array;
+}
+
+void Show2dArray(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array [i,j] + "\t");  
+        Console.WriteLine();
+    }
+ Console.WriteLine();   
+}
+
+double[,] newArray = Random2dArray ();
+Show2dArray(newArray);
+Console.Write("Input the number of row: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the number of column: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n > newArray.GetLength(0) || m > newArray.GetLength(1))
+    Console.WriteLine("There is no such element.");
+else
+    Console.WriteLine($"The value of the row {n} and column {m} element is {newArray[n-1,m-1]}.");
 
 
 /*Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов
